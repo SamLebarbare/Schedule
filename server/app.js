575857856e99879b5ac1e02386e3ebb7e7c5ef69
Samuel Loup <samuel.loup@gmail.com>
2015-11-20 14:34:48 +0100

    Next step...

--- server/app.js
+++ server/app.js
@@ -9,24 +9,32 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var mongoose = require('mongoose');
-var config = require('./config/environment');
+var config = require('./config/environment/');
+var User   = require('./api/user/user.model');
+// Setup server
+var app = express();
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
-// Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
 
-// Setup server
-var app = express();
-var server = require('http').createServer(app);
-require('./config/express')(app);
-require('./routes')(app);
 
-// Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+function bootstrap (err, nbOfUsers) {
+  if (nbOfUsers === 0) {
+    require('./config/seedProd');
+  }
+
+  var server = require('http').createServer(app);
+  require('./config/express')(app);
+  require('./routes')(app);
+
+  // Start server
+  server.listen(config.port, config.ip, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
+}
+
+User.count ({}, bootstrap);
 
 // Expose app
 exports = module.exports = app;
